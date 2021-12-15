f17dd44c1383c982dee24ddd202eb6d832d390f2
Manuel <manuelzuletab@gmail.com>
2021-12-15 00:06:25 -0300

    push inicial

---
+++ resources/views/comida/palta.blade.php
@@ -0,0 +1,30 @@
+
+    <html>
+        <head>
+        </head>
+        <body>
+            Prueba
+            <div class="asd">
+                <p id="texto"></p>
+                <p id="texto2"></p>
+            </div>
+        </body>
+    </html>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            fetch('/listar_paltas',{
+                method: 'GET',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+                },
+                credentials: 'same-origin',
+            }).then(function(response){
+                response.json().then(function(data){
+                    console.log(data);
+                    $('.asd #texto').text(data.hambre);
+                    $('.asd #texto2').text(data.comida);
+                });
+            });
+        });
+    </script>
